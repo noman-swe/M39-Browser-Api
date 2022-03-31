247baefaa1f49b1ca8d3d7f22bdcbc1d08852823
noman-swe <noman.swe130@gmail.com>
2022-03-31 23:36:48 +0600

    39-1 Module Overview, BOM vs DOM, Browser API

---
+++ explore.js
@@ -0,0 +1,9 @@
+const name = 15;
+const student = {name: 'jolil', friend: 'Borsha', job: 'Business',
+makeMovie:function(name){
+    console.log(name);
+}
+}
+
+student.makeMovie('Din Raat');
+document.getElementById('name');
